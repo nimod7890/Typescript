74b757314e3d8a3936bfdf8ad3aaeddd22139072
nimod7890 <nimod1234@g.skku.edu>
2022-04-19 14:06:33 +0900

    clone repository

---
+++ why-ts/app.js
@@ -0,0 +1,24 @@
+// api url
+var url = "https://jsonplaceholder.typicode.com/users/1";
+
+// dom
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
+
+// user data
+var user = {};
+
+function startApp() {
+  axios
+    .get(url)
+    .then(function (response) {
+      console.log(response);
+      user = response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
